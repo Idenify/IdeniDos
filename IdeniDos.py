f367afc640cb2c905ac46b2dcf87a601cf98bcb3
Idenify <45737924+Idenify@users.noreply.github.com>
2018-12-21 13:37:05 +0100

    Add files via upload

--- IdeniDos.py
+++ IdeniDos.py
@@ -3,14 +3,17 @@ from threading import Thread
 from colorama import init, Fore, Style
 init()
 
-print(Fore.RED + ".___    .___            .__  _____       ") 	
-print(Fore.RED + "|   | __| _/____   ____ |__|/ ____\__.__. ")	
-print(Fore.RED + "|   |/ __ |/ __ \ /    \|  \   __<   |  | ")	
-print(Fore.RED + "|   / /_/ \  ___/|   |  \  ||  |  \___  | ")	
-print(Fore.RED + "|___\____ |\___  >___|  /__||__|  / ____| ")	
-print(Fore.RED + "               \/    \/     \/    \/      ")     
-print(Fore.GREEN + "IdeniDos v2.0")
-print(Fore.GREEN + "YouTube: Idenify")
+print(Fore.RED + ".___    .___            .__  _____   		        ") 	
+print(Fore.RED + "|   | __| _/____   ____ |__|/ ____\__.__.             ")	
+print(Fore.RED + "|   |/ __ |/ __ \ /    \|  \   __<   |  |             ")	
+print(Fore.RED + "|   / /_/ \  ___/|   |  \  ||  |  \___  |             ")	
+print(Fore.RED + "|___\____ |\___  >___|  /__||__|  / ____| 	     	")	
+print(Fore.RED + "               \/    \/     \/    \/      v2.0     	")     
+print(Fore.RED + "						    	")
+print(Fore.RED + "   IdeniDos v2.0				   	")
+print(Fore.RED + "    YouTube: Idenify				    	")
+print(Fore.RED + "						     	")
+print(Fore.GREEN + "")
 print 
 print 
 host = raw_input("IP Target : ")
